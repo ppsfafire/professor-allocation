bbaca032c7e6d1096d965b32487fd0341491467e
Carlos Torres <carlostorressjrdev@gmail.com>
2025-02-22 16:37:34 -0300

    Feat: Implementation class

---
+++ src/main/java/com/project/professor/allocation/entity/Professor.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Professor {
+	private Long id;
+	private String name;
+	private String cpf;
+
+}
