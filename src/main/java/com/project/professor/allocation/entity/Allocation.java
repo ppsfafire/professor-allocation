56db8b8c1c62a91ed7d2d1f180f81f55c51ed06c
Isis Xavier <isisxavierti@gmail.com>
2025-03-08 09:30:45 -0300

    Att: código

--- src/main/java/com/project/professor/allocation/entity/Allocation.java
+++ src/main/java/com/project/professor/allocation/entity/Allocation.java
@@ -1,5 +1,20 @@
 package com.project.professor.allocation.entity;
 
+import java.sql.Time;
+import java.time.DayOfWeek;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Allocation {
+	
+	private Long id;
+	private DayOfWeek dayOfWeek;
+	private Time startHour;
+	private Time endHour;
+	
+	
 
 }
