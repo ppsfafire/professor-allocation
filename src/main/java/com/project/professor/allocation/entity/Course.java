282db52fce6ab6096ece6d5cd97a9db0babc92b1
Carlos Torres <carlostorressjrdev@gmail.com>
2025-03-08 10:04:26 -0300

    Auto stash before merge of "main" and "origin/main"

--- src/main/java/com/project/professor/allocation/entity/Course.java
+++ src/main/java/com/project/professor/allocation/entity/Course.java
@@ -1,6 +1,16 @@
 package com.project.professor.allocation.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Data
+@AllArgsConstructor
 public class Course {
+	
+	@Id
 	private Long id;
 	private String name;
 	private String description;
