341d1adea3ab89f50fc0664376faf85403495457
Carlos Torres <carlostorressjrdev@gmail.com>
2025-03-22 14:43:42 -0300

    Feat: @PutMapping

--- src/main/java/com/project/professor/allocation/controller/AllocationController.java
+++ src/main/java/com/project/professor/allocation/controller/AllocationController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,6 +43,15 @@ public class AllocationController {
 	public ResponseEntity<Allocation> save(@RequestBody Allocation allocation) {
 		allocation.setId(100L);
 		return new ResponseEntity<>(allocation, HttpStatus.CREATED);
+	}
+	
+	@PutMapping (path = "/{allocation_id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Allocation> update(@PathVariable(name = "allocation_id") Long id, @RequestBody Allocation allocation) {
+		
+		allocation.setId(200L);
+		allocation.setDayOfWeek(null);
+		return new ResponseEntity<>(allocation, HttpStatus.OK);
+	
 	}
 	
 	@DeleteMapping(path = "/{allocation_id}")
