f290a324e6b32ca89d7540ad21a3c62c95784fb7
Carlos Torres <carlostorressjrdev@gmail.com>
2025-03-22 14:13:02 -0300

    add: @DeleteMapping

--- src/main/java/com/project/professor/allocation/controller/AllocationController.java
+++ src/main/java/com/project/professor/allocation/controller/AllocationController.java
@@ -9,7 +9,9 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,6 +44,19 @@ public class AllocationController {
 		return new ResponseEntity<>(allocation, HttpStatus.CREATED);
 	}
 	
+	@DeleteMapping(path = "/{allocation_id}")
+	public ResponseEntity<Void> deleteById(@PathVariable(name = "allocation_id") Long id) {
+		
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		
+	}
+	
+	@DeleteMapping(path = "/{allocation_id}")
+	public ResponseEntity<Void> deleteAll() {
+		
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		
+	}
 	
 }
 
